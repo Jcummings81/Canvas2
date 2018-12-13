d4fb86a65fab901bc165fe3f138fe8d8d633adc4
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-12-13 12:42:22 -0700

    nested enrollments routes inside courses and added enrollments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'enrollments/index'
+  get 'enrollments/new'
   #localhost:3000
   root 'courses#index'
 
   resources :users
-  resources :courses
+  resources :courses do 
+    resources :enrollments, only: [:index, :new, :create, :destroy ]
+  end
 end
