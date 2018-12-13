565c39f6b25719433b17e583e6d99fd4b9e5822f
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-12-13 11:46:26 -0700

    finished courses controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,13 +13,11 @@ class UsersController < ApplicationController
 
   # GET /users/new
   def new
-    binding.pry
     @user = User.new
   end
 
   # POST /users
   def create 
-    binding.pry
     @user = User.new(user_params)
 
     if @user.save
